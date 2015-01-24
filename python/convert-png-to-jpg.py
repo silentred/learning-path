08ff59c5b3a0a3f18143928e604145bbe4a51b9d
silentred <silentred3@gmail.com>
2015-01-24 14:57:16 +0800

    dd

---
+++ python/convert-png-to-jpg.py
@@ -0,0 +1,11 @@
+from PIL import Image
+import glob
+
+#im = Image.open(r"C:\jk.png")
+#bg = Image.new("RGB", im.size, (255,255,255))
+#bg.paste(im,im)
+#bg.save(r"C:\jk2.jpg")
+
+print glob.glob('E:/lifome/product-img-edited/*.png')
+
+
