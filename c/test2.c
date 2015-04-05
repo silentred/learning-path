20fe014de2cdc5cdc4b93e73db4980cd34457a8b
silentred <silentred3@gmail.com>
2015-04-05 02:44:14 -0400

    c

---
+++ c/test2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+/* function declaration  */
+void func(void);
+ 
+static int count = 5; /* global variable */
+
+int main()
+{
+   /* my first program in C */
+   printf("Hello, World! \n");
+   printf("SCHAR_MIN, SCHAR_MAX, SCHAR_MAX:%d, %d, %d . \n",SCHAR_MIN, SCHAR_MAX, SCHAR_MAX);
+   printf("SHRT_MIN, SHRT_MAX, SHRT_MAX:%d, %d, %d . \n",SHRT_MIN, SHRT_MAX, SHRT_MAX);
+   printf("INT_MIN, INT_MAX, UINT_MAX:%d, %d, %d . \n",INT_MIN, INT_MAX, UINT_MAX);
+   printf("LONG_MIN, LONG_MAX, LONG_MAX:%ld, %ld, %ld . \n",LONG_MIN, LONG_MAX, LONG_MAX);
+   printf("%d \n", '\60');
+
+   char *msg = "hello world"; //msg是指向第一个字符“h”的指针；这里是对msg赋值而不是对*msg赋值，和下面的例子等价。
+   printf("char:%c point: %p\n",*(msg+1), msg);
+
+   //上面的方式容易让人误解，以为"hello"赋给了*msg，其实不是的，"hello"赋给了msg。
+   char *message;
+   message = "Hello !!!";  //message是字符指针，字符串字面量的直接值也是指针，所以可以这样赋值；
+   printf("char:%s point: %p\n",message, message);
+
+   return 0;
+}
+
+
---
+++ js/test.js
@@ -0,0 +1,11 @@
+var a = {n: 1}
+var b = a;
+a.x = a = {n: 2}
+console.log(a.x);
+console.log(b.x)
+
+var F = function(){};
+Object.prototype.a = function(){};
+Function.prototype.b = function(){};
+var f = new F();
+他这里想问的是f能不能拿到a方法和b方法，我这里知道有坑，我也知道能拿到a，但是我回答的是能拿到a和b，他说不对，然后就过了，我觉得这里是我表达不对，因为在读Backbone源码的时候，我使用过f.constructor可以同时拿到a和b，结果他以为我认为直接通过f拿
