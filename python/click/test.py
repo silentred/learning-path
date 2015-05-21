920ebc9fed220f577eda12404d8faaa1f8eb91f8
silentred <silentred3@gmail.com>
2015-05-21 23:21:42 +0800

    'p'

--- python/click/test.py
+++ python/click/test.py
@@ -3,6 +3,37 @@
 
 import click
 
-@click.command
-def hello():
-    click.echo("hello world")
+@click.group(invoke_without_command=True)
+@click.option('--count', default=1, help='number of greetings')
+@click.argument('name')
+@click.pass_context
+def cli(ctx, **kwargs):
+    print ctx
+    print kwargs
+    for x in range(kwargs['count']):
+        click.echo('Hello %s!' % kwargs['name'])
+    print ctx.invoked_subcommand
+    print ctx.args
+    ctx.obj = {"myObj": "awsome"}
+    ctx.invoke(hello2)
+    print "after invoking"
+    #return ctx
+
+@cli.command()
+@click.pass_context
+def hello2(ctx):
+    print ctx.obj
+    print "hello2!!!"
+
+@cli.command()
+def initdb():
+    click.echo('Initialized the database')
+
+@cli.command()
+def dropdb():
+    click.echo('Dropped the database')
+
+
+
+if __name__ == '__main__':
+    cli()
