f385e55f03ed7af2302dec6917fa55466aadc0b4
silentred <silentred3@gmail.com>
2015-08-02 21:52:50 +0800

    sdf

--- golang/http.go
+++ golang/http.go
@@ -1,15 +1,29 @@
 package main
 
 import (
-    "io"
-    "net/http"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-    io.WriteString(w, "Hello world!")
+func handlerA(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello world!")
+}
+
+func handlerB(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+	myItems := []string{"item1", "item2", "item3"}
+	a, _ := json.Marshal(myItems)
+
+	fmt.Println("hello")
+
+	w.Write(a)
+	return
 }
 
 func main() {
-    http.HandleFunc("/", hello)
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", handlerB)
+	http.ListenAndServe(":8080", nil)
 }
