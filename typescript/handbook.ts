473a13ad1f2ce6950c00bb9120cdbf3cf71ba8df
Jason W <silentred@163.com>
2015-08-17 13:54:02 +0800

    type script ++

---
+++ typescript/handbook.ts
@@ -0,0 +1,12 @@
+// handbook
+
+// Type
+var boolType : boolean = false;
+var numberType : number = 2;
+var stringType : string = "HEllo";
+
+var list: number[] = [2,3,5];
+var list2 : Array<number> = [3,5,6];
+
+enum Color {Red = 2, Green, Yellow};
+var red : Color = Color.Red;
