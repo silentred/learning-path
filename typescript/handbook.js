b05d3dba1365eb0798e06c6658003c13046bf7f4
Jason W <silentred@163.com>
2015-09-05 20:16:21 +0800

    ts

--- typescript/handbook.js
+++ typescript/handbook.js
@@ -13,3 +13,70 @@ var Color;
 })(Color || (Color = {}));
 ;
 var red = Color.Red;
+var whatever = 4;
+whatever = "string";
+// Classes 
+var Greeter = (function () {
+    function Greeter(message) {
+        this.greeting = message;
+    }
+    Greeter.prototype.greet = function () {
+        return "Hello, " + this.greeting;
+    };
+    return Greeter;
+})();
+var greeter = new Greeter("world");
+// Use `extends` for inheritance
+// 
+// `private` property
+// 
+//  public/private 可以写在constructor中表示创建property。
+var Animal = (function () {
+    function Animal(name) {
+        this.name = name;
+    }
+    Animal.prototype.move = function (meters) {
+        alert(this.name + " moved " + meters + "m.");
+    };
+    return Animal;
+})();
+// Accessors
+var passcode = "secret passcode";
+var Employee = (function () {
+    function Employee() {
+    }
+    Object.defineProperty(Employee.prototype, "fullName", {
+        get: function () {
+            return this._fullName;
+        },
+        set: function (newName) {
+            if (passcode && passcode == "secret passcode") {
+                this._fullName = newName;
+            }
+            else {
+                alert("Error: Unauthorized update of employee!");
+            }
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Employee;
+})();
+var employee = new Employee();
+employee.fullName = "Bob Smith";
+if (employee.fullName) {
+    alert(employee.fullName);
+}
+// static property
+var Grid = (function () {
+    function Grid(scale) {
+        this.scale = scale;
+    }
+    Grid.prototype.calculateDistanceFromOrigin = function (point) {
+        var xDist = (point.x - Grid.origin.x);
+        var yDist = (point.y - Grid.origin.y);
+        return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale;
+    };
+    Grid.origin = { x: 0, y: 0 };
+    return Grid;
+})();
--- typescript/handbook.ts
+++ typescript/handbook.ts
@@ -10,3 +10,111 @@ var list2 : Array<number> = [3,5,6];
 
 enum Color {Red = 2, Green, Yellow};
 var red : Color = Color.Red;
+
+var whatever: any = 4;
+whatever = "string";
+
+// Interface
+
+// optional property
+interface SquareConfig {
+  color?: string;
+  width?: number;
+}
+
+// function
+interface SearchFunc {
+  (source: string, subString: string): boolean;
+}
+
+// array
+interface StringArray {
+  [index: number]: string;
+}
+
+// class
+interface ClockInterface {
+    currentTime: Date;
+    setTime(d: Date);
+}
+
+// static class (need to search)
+
+// extend interface
+interface Shape {
+    color: string;
+}
+
+interface PenStroke {
+    penWidth: number;
+}
+
+interface Square extends Shape, PenStroke {
+    sideLength: number;
+}
+
+// Classes 
+
+class Greeter {
+    greeting: string;
+    constructor(message: string) {
+        this.greeting = message;
+    }
+    greet() {
+        return "Hello, " + this.greeting;
+    }
+}
+
+var greeter = new Greeter("world");
+
+// Use `extends` for inheritance
+// 
+// `private` property
+// 
+//  public/private 可以写在constructor中表示创建property。
+class Animal {
+    constructor(private name: string) { }
+    move(meters: number) {
+        alert(this.name + " moved " + meters + "m.");
+    }
+} 
+
+// Accessors
+var passcode = "secret passcode";
+
+class Employee {
+    private _fullName: string;
+
+    get fullName(): string {
+        return this._fullName;
+    }
+    
+    set fullName(newName: string) {
+        if (passcode && passcode == "secret passcode") {
+            this._fullName = newName;
+        }
+        else {
+            alert("Error: Unauthorized update of employee!");
+        }
+    }
+}
+
+var employee = new Employee();
+employee.fullName = "Bob Smith";
+if (employee.fullName) {
+    alert(employee.fullName);
+}
+
+// static property
+class Grid {
+    static origin = {x: 0, y: 0};
+    calculateDistanceFromOrigin(point: {x: number; y: number;}) {
+        var xDist = (point.x - Grid.origin.x);
+        var yDist = (point.y - Grid.origin.y);
+        return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale;
+    }
+    constructor (public scale: number) { }
+}
+
+
+
