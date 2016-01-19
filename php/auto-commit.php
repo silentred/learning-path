9e4c343515796c2890e2fed1e8e5681f967f1f7c
Jason W <silentred3@gmail.com>
2016-01-19 20:08:31 +0800

    2016-01-19

---
+++ php/auto-commit.php
@@ -0,0 +1,11 @@
+<?php
+
+$fd = fopen(__FILE__, 'a');
+$len = fwrite($fd, "\n");
+fclose($fd);
+
+chdir(__DIR__.'/../');
+exec("git add *");
+$today = date("Y-m-d");
+exec("git commit -m \"{$today}\" ");
+exec("git push");
