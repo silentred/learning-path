03deb6c3df14bf260116a9fab6d1b2a2ae23146f
Jason W <silentred3@gmail.com>
2016-10-27 21:07:15 +0800

    change docker test

--- software/docker/swarm/main.go
+++ software/docker/swarm/main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 )
 
+var (
+	host, port string
+)
+
 func main() {
+	flag.StringVar(&host, "host", "", "host usage")
+	flag.StringVar(&port, "port", "9090", "host usage")
+	flag.Parse()
+
+	fmt.Println(host, port)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 	})
--- software/docker/swarm/swarm.md
+++ software/docker/swarm/swarm.md
@@ -9,3 +9,15 @@ docker ps -a
 docker swarm init
 docker service create --publish 9090:9090 --name hello-service jason/hello:v1
 
+
+## 不使用集群管理工具
+
+### 手动指定 Host Address
+docker run -p 9090:9090 -d image:tag --host xxx 
+
+### docker API 获取 port 信息
+docker run -P -d image:tag --host xxx
+或者 host 可以 从 ENV 传入容器内，一台物理机只需要配置一次 env
+
+## 使用 k8s
+
