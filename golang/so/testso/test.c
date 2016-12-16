0fdcb1fecfd31f8a7044808ac86a77914124e79d
Jason W <silentred3@gmail.com>
2016-12-16 18:10:19 +0800

    go build so file and cpp use it

---
+++ golang/so/testso/test.c
@@ -0,0 +1,10 @@
+#include "testso.h"
+#include <stdio.h>
+
+int main() {
+  printf("This is a C Application.\n");
+  GoString name = {"Jack", 4};
+  SayHello(name);
+  SayBye();
+  return 0;
+}
---
+++ golang/so/testso/test.cpp
@@ -0,0 +1,12 @@
+#include "testso.h"
+#include <iostream>
+
+int main() {
+    std::cout << "This is a Cpp Application." << std::endl;
+  
+    GoString name = {"Jack", 4};
+    SayHello(name);
+    SayBye();
+
+    return 0;
+}
