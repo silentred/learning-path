80a7d5ca57beebb71068aa33ec55bdbcc271af8f
Jason W <silentred@163.com>
2017-06-18 00:36:17 +0800

    add post curl

--- php/fetch-comic-id.php
+++ php/fetch-comic-id.php
@@ -3,7 +3,7 @@
 $filePath = '/tmp/comics.txt';
 $fd = fopen($filePath, 'a+');
 
-for ($i=1000; $i < 5000; $i++) { 
+for ($i=15000; $i < 20000; $i++) { 
     $url = sprintf("http://v2.api.dmzj.com/comic/%d.json", $i);
     $data = httpGet($url);
     $obj = json_decode($data, true);
@@ -27,3 +27,29 @@ function httpGet($url) {
 	return $data;
 }
 
+function httpPost($url, $fields){
+	//url-ify the data for the POST
+	foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+	rtrim($fields_string, '&');
+	//open connection
+	$ch = curl_init();
+	curl_setopt($ch,CURLOPT_URL, $url);
+	curl_setopt($ch,CURLOPT_POST, count($fields));
+	curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+	$result = curl_exec($ch);
+	curl_close($ch);
+}
+
+function sub($uid, $comicID){
+	$url = 'http://v2.api.dmzj.com/subscribe/add';
+	//channel=ios&obj_ids=12090&type=mh&uid=102198270&version=2.2.4
+	$fields = [
+		'channel' => 'ios',
+		'obj_ids' => $comicID,
+		'type' => 'mh',
+		'uid' => $uid,
+		'version' => '2.2.4',
+	];
+
+	httpPost($url, $fields);
+}
