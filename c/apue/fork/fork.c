c8def974658d965615cc7f36240dfd77410e507a
JasonW <silentred3@gmail.com>
2017-08-04 12:05:37 +0800

    fork

--- c/apue/fork/fork.c
+++ c/apue/fork/fork.c
@@ -1,19 +1,37 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <sys/wait.h>
 
 int main() {
     pid_t pid;
+    int fd;
+    int num = 917;
 
-    pid = fork();
-    
+    // create shared mem
+    fd = shm_open("/test", O_CREAT | O_RDWR, 0666);
+    ftruncate(fd, sizeof(int));
+    write(fd, &num, sizeof(int));
+    printf("fd=%d \n", fd);
+
+    pid = fork();    
     if (pid > 0) {
         // parent
         printf("parent \n");
+        if (waitpid(pid, NULL, 0) != pid) {
+            printf("waitpid error\n");
+        }
     } else if (pid == 0) {
         // child
-        execv("./echo", NULL);
+        //execv("./echo", NULL);
+        int newNum;
+        ssize_t readsize;
+        fd = shm_open("/test",  O_RDWR, 0666);
+        readsize = read(fd, &newNum, sizeof(int));
+        printf("child, fd=%d, num=%d \n", fd, newNum);
         sleep(1);
     } else {
-        printf("for error \n");
+        printf("fork error \n");
     }
 }
