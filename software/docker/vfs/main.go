b0843f8c8ff29f37b0d0bf80f309ab6aac0f203a
Jason W <silentred3@gmail.com>
2017-09-01 10:36:45 +0800

    vfs test

---
+++ software/docker/vfs/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
+
+const (
+	pageSize  = 4 << 10   // 4k
+	totalSize = 300 << 20 // 300mb
+)
+
+var (
+	filePath = "/tmp/data"
+	content  = strings.Repeat("a", pageSize)
+)
+
+func main() {
+	file, err := os.Create(filePath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// write test
+	var bytesWriten int
+	var timeStart = time.Now()
+	for bytesWriten < totalSize {
+		n, err := file.WriteString(content)
+		if err != nil {
+			log.Printf("write error: %v \n", err)
+		}
+		bytesWriten += n
+	}
+	err = file.Sync()
+	if err != nil {
+		log.Printf("sync error: %v \n", err)
+	}
+	duration := time.Now().Sub(timeStart)
+	file.Close()
+	fmt.Printf("Write %d bytes, cost %s \n", totalSize, duration)
+
+	// read test
+	var buf = make([]byte, pageSize)
+	file, err = os.Open(filePath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	var bytesRead int
+	timeStart = time.Now()
+	for bytesRead < totalSize {
+		n, err := file.Read(buf)
+		if err != nil {
+			log.Printf("write error: %v \n", err)
+		}
+		bytesRead += n
+	}
+	duration = time.Now().Sub(timeStart)
+	file.Close()
+	fmt.Printf("Read %d bytes, cost %s \n", totalSize, duration)
+}
