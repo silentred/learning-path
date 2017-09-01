206b51e41b7c4bea2f63f26538462cc4a75fe649
Jason W <silentred3@gmail.com>
2017-09-01 12:08:13 +0800

    change

--- software/docker/vfs/main.go
+++ software/docker/vfs/main.go
@@ -14,63 +14,81 @@ const (
 )
 
 var (
-	totalSize = 1 << 30 // 1G
-	filePath  = "/tmp/data"
-	content   = strings.Repeat("a", pageSize)
+	totalSize   = 1 << 30 // 1G
+	filePath    = "/tmp/data"
+	imgFilePath = "/tmp/output.dat"
+	content     = strings.Repeat("a", pageSize)
 )
 
 func main() {
 	flag.IntVar(&totalSize, "size", totalSize, "total size to write")
 	flag.Parse()
 
-	file, err := os.Create(filePath)
+	duration := write(filePath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, totalSize)
+	fmt.Printf("Write %d bytes to wr-layer, cost %s \n", totalSize, duration)
+
+	duration = read(filePath, os.O_WRONLY, totalSize)
+	fmt.Printf("Read %d bytes from wr-layer, cost %s \n", totalSize, duration)
+
+	duration = append(imgFilePath, 500)
+	fmt.Printf("[Img] Append %d times, cost %s \n", 500, duration)
+	duration = read(imgFilePath, os.O_WRONLY, totalSize)
+	fmt.Printf("[Img] Read %d bytes from image-layer, cost %s \n", 300<<20, duration)
+}
+
+func write(fileName string, flag, total int) time.Duration {
+	file, err := os.OpenFile(fileName, flag, 0600)
+	defer file.Close()
+
 	if err != nil {
 		log.Fatalln(err)
 	}
-
-	// write test
 	var bytesWriten int
 	var timeStart = time.Now()
-	for bytesWriten < totalSize {
+	for bytesWriten < total {
 		n, err := file.WriteString(content)
 		if err != nil {
 			log.Printf("write error: %v \n", err)
+			break
 		}
 		bytesWriten += n
 	}
 	duration := time.Now().Sub(timeStart)
-	file.Close()
-	fmt.Printf("Write %d bytes, cost %s \n", totalSize, duration)
+	return duration
+}
 
-	// append to file
-	timeStart = time.Now()
-	file, err = os.OpenFile(filePath, os.O_APPEND|os.O_WRONLY, 0600)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	for i := 0; i < 100; i++ {
-		file.WriteString("test")
-	}
-	duration = time.Now().Sub(timeStart)
-	file.Close()
-	fmt.Printf("Append 100 times, cost %s \n", duration)
-
-	// read test
+func read(fileName string, flag, total int) time.Duration {
 	var buf = make([]byte, pageSize)
-	file, err = os.Open(filePath)
+	file, err := os.OpenFile(fileName, flag, 0600)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	var bytesRead int
-	timeStart = time.Now()
-	for bytesRead < totalSize {
+	timeStart := time.Now()
+	for bytesRead < total {
 		n, err := file.Read(buf)
 		if err != nil {
 			log.Printf("write error: %v \n", err)
+			break
 		}
 		bytesRead += n
 	}
-	duration = time.Now().Sub(timeStart)
+	duration := time.Now().Sub(timeStart)
 	file.Close()
-	fmt.Printf("Read %d bytes, cost %s \n", totalSize, duration)
+	return duration
+}
+
+func append(fileName string, cnt int) time.Duration {
+	// append to file
+	timeStart := time.Now()
+	file, err := os.OpenFile(imgFilePath, os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for i := 0; i < cnt; i++ {
+		file.WriteString(content)
+	}
+	duration := time.Now().Sub(timeStart)
+	file.Close()
+	return duration
 }
